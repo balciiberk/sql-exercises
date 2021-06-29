f74f571ad614f71fc257fbad6a2b980c2dcce5bc
berk <balciiberk@gmail.com>
2021-06-29 18:41:23 +0300

    added 12th exercise

---
+++ exercises12.sql
@@ -0,0 +1,59 @@
+-- Q1
+SELECT COUNT(*) FROM film
+WHERE length > 
+(
+	SELECT AVG(length) FROM film
+);
+
+-- Q2
+SELECT COUNT(*) FROM film
+WHERE rental_rate =
+(
+	SELECT MAX(rental_rate) FROM film
+);
+
+-- Q3
+SELECT * FROM film
+WHERE rental_rate = 
+(
+	SELECT MIN(rental_rate) FROM film
+)
+AND
+replacement_cost = 
+(
+	SELECT MIN(replacement_cost) FROM film
+);
+
+-- Q3 (alternative to use ALL/ANY)
+SELECT * FROM film
+WHERE film_id = ANY
+(
+	SELECT film_id FROM film
+	WHERE rental_rate =
+	(
+	SELECT MIN(rental_rate) FROM film
+	)
+	AND
+	replacement_cost = 
+	(
+	SELECT MIN(replacement_cost) FROM film
+	)
+);
+
+-- Q4
+SELECT * FROM customer
+WHERE customer_id = ANY
+(
+	SELECT customer_id FROM payment
+	GROUP BY customer_id
+	HAVING COUNT(*) =
+	(
+		SELECT MAX(count_nums) FROM 
+		(
+			SELECT customer_id, COUNT(*) AS count_nums FROM payment
+			GROUP BY customer_id
+		) AS counts
+	)
+)
+
+
